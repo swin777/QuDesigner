e77f2ab3b0cd9bddd713ea13033f5bac12cf64a3
ash <ash@192.168.35.42>
2015-09-21 15:52:47 +0900

    ..

--- QuDesigner/ashDraw/Figure.js
+++ QuDesigner/ashDraw/Figure.js
@@ -397,6 +397,9 @@ define(["dojo/_base/declare",
 	            this.x = Math.round(tmpX) * 10;
 	            this.y = Math.round(tmpY) * 10;
 	        }
+	        if(this.command){
+	        	return;
+	        }
 	        this.command.setPosition(this.x, this.y);
 	        this.isInDragDrop = false;
 	
